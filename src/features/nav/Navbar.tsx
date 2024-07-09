e24f58c99f00418a56262bc0ea53891a67909a31
Perez Githumbi <codakniggz145@gmail.com>
2024-07-09 17:22:45 +0300

    Replaced hard coded route paths with abstracted ones

--- src/features/nav/Navbar.tsx
+++ src/features/nav/Navbar.tsx
@@ -1,32 +1,32 @@
-import React from "react";
 import Dropdown from "./Dropdown";
 import Search from "./Search";
 import Notif from "./Notif";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import { about, cv, portfolio } from "../../routePaths";
 
-type Props = {
-  children: React.ReactNode;
-};
-const Navbar = ({ children }: Props) => {
+const Navbar = () => {
   return (
     <>
       <div className="navbar bg-base-100">
         <div className="navbar-start">
           <Dropdown>
+            {/* <li>
+              <Link to={homepage}>homepage</Link>
+            </li> */}
             <li>
-              <Link to="/homepage">homepage</Link>
+              <Link to={cv}>Resume</Link>
             </li>
             <li>
-              <Link to="/portfolio">portfolio</Link>
+              <Link to={portfolio}>Portfolio</Link>
             </li>
             <li>
-              <Link to="/about">about</Link>
+              <Link to={about}>About</Link>
             </li>
           </Dropdown>
         </div>
         <div className="navbar-center">
           <Link to="/homepage" className="btn btn-ghost text-xl text-zinc-700">
-            Project
+            Resume Builder
           </Link>
         </div>
         <div className="navbar-end">
@@ -36,7 +36,9 @@ const Navbar = ({ children }: Props) => {
           </div>
         </div>
       </div>
-      <div>{children}</div>
+      <div>
+        <Outlet />
+      </div>
     </>
   );
 };
