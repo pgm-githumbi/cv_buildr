eb73e1978ccfb18aeabca49708b30e79ad52d589
Perez Githumbi <codakniggz145@gmail.com>
2024-07-09 17:13:07 +0300

    Centralized route paths

---
+++ src/routePaths.js
@@ -0,0 +1,12 @@
+export const homepage = "/homepage";
+export const about = "/about";
+export const portfolio = "/portfolio";
+export const search = "/search";
+
+export const cv = `cv`;
+export const cvEditHeader = `head`;
+export const cvEditSummary = `summary`;
+export const cvProfessionalExperience = "experience";
+export const cvEditEducation = "education";
+export const cvEditKeySkills = "key_skills";
+export const cvEditProjects = "projects";
