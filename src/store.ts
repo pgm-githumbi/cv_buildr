f65bca400beb57b116820a9aa1ca60fb8e3ff443
Perez Githumbi <codakniggz145@gmail.com>
2024-07-09 17:15:36 +0300

    registered redux slices

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import NavbarSlice from "./redux/ui/NavbarSlice";
+import HeaderSlice from "./redux/cv/HeaderSlice";
+import SummarySlice from "./redux/cv/SummarySlice";
+import ExperienceSlice from "./redux/cv/ExperienceSlice";
 
 const store = configureStore({
-  reducer: { navbar: NavbarSlice },
+  reducer: {
+    navbar: NavbarSlice,
+    cvHeader: HeaderSlice,
+    summary: SummarySlice,
+    experience: ExperienceSlice,
+  },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
