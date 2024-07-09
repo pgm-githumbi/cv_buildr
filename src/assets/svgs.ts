aeeca642112cb3e8e9c690c687735195ab81dfb6
Perez Githumbi <codakniggz145@gmail.com>
2024-07-09 17:48:29 +0300

    Centralizing svgs

---
+++ src/assets/svgs.ts
@@ -0,0 +1,5 @@
+import { ReactComponent as DateSvg } from "../features/cv/date.svg";
+import { ReactComponent as EmailSvg } from "../features/cv/email.svg";
+import { ReactComponent as JobSvg } from "../features/cv/job_icon.svg";
+import { ReactComponent as PhoneSvg } from "../features/cv/phone_number.svg";
+import { ReactComponent as UserSvg } from "../features/cv/username_logo.svg";
