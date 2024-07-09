3bfe351337907da6a40298821dbbfe156ba9b2f7
Perez Githumbi <codakniggz145@gmail.com>
2024-07-09 17:10:22 +0300

    Added flowbite plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui"),
+    // require("flowbite/plugin"),
+  ],
   daisyui: { themes: ["winter"] },
 };
